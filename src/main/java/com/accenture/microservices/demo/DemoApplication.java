8704c70b682f862c5a5177cad0977643dd42fc5e
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-10-23 17:50:24 +0200

    Remove configuration class

--- src/main/java/com/accenture/microservices/demo/DemoApplication.java
+++ src/main/java/com/accenture/microservices/demo/DemoApplication.java
@@ -3,10 +3,8 @@ package com.accenture.microservices.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
