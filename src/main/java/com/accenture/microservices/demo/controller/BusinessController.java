d61ccff67e4a861caaba16f6a3539e1ef0845500
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-10-23 17:51:15 +0200

    Reformat

--- src/main/java/com/accenture/microservices/demo/controller/BusinessController.java
+++ src/main/java/com/accenture/microservices/demo/controller/BusinessController.java
@@ -1,8 +1,12 @@
 package com.accenture.microservices.demo.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,9 +16,28 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 public class BusinessController
 {
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping("/businesses")
-    public ResponseEntity<List<String>> getBusinesses()
+    public TestEntity getBusinesses()
     {
-        return ok(Arrays.asList("strategy", "consulting", "digital", "technology", "operations"));
+//        return ok(Arrays.asList("strategy", "consulting", "digital", "technology", "operations"));
+        TestEntity testEntity = new TestEntity();
+        testEntity.setName("Hello world!");
+        return testEntity;
+    }
+
+    public static class TestEntity
+    {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
