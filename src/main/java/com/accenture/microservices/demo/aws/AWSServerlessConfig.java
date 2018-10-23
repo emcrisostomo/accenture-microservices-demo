1f9de72e8fbea4180dea5fda72888311177b2ca1
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-10-23 17:48:22 +0200

    Remove bean overrides to avoid MVC autoconfiguration to be wiped away

--- src/main/java/com/accenture/microservices/demo/aws/AWSServerlessConfig.java
+++ src/main/java/com/accenture/microservices/demo/aws/AWSServerlessConfig.java
@@ -4,30 +4,11 @@ import com.accenture.microservices.demo.Profiles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
 @Profile(Profiles.PROFILE_AWS_SERVERLESS)
 public class AWSServerlessConfig {
-    /*
-     * Create required HandlerMapping, to avoid several default HandlerMapping instances being created
-     */
-    @Bean
-    public HandlerMapping handlerMapping() {
-        return new RequestMappingHandlerMapping();
-    }
-
-    /*
-     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
-     */
-    @Bean
-    public HandlerAdapter handlerAdapter() {
-        return new RequestMappingHandlerAdapter();
-    }
 
     /*
      * optimization - avoids creating default exception resolvers; not required as the serverless container handles
