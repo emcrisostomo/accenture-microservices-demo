738df599eb05a449bdc9ac7c31d54754782677d4
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-10-23 23:10:26 +0200

    Parametrize aws-refresh-stack.sh

--- aws-refresh-stack.sh
+++ aws-refresh-stack.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+set -e
+
+STACK_NAME=spring-boot-on-lambda-demo
+REPO_ROOT=$(dirname $0)
+
+cd ${REPO_ROOT}
 ./mvnw -P aws-serverless -D skipTests=true clean package
-aws cloudformation delete-stack --stack-name spring-boot-on-lambda-demo
-aws cloudformation package --template-file sam.yaml --output-template-file target/output-sam.yaml --s3-bucket spring-boot-on-lambda-demo
-aws cloudformation deploy --template-file target/output-sam.yaml --stack-name spring-boot-on-lambda-demo --capabilities CAPABILITY_IAM
-aws cloudformation describe-stacks --stack-name spring-boot-on-lambda-demo
+aws cloudformation delete-stack --stack-name ${STACK_NAME}
+aws cloudformation package --template-file sam.yaml --output-template-file target/output-sam.yaml --s3-bucket ${STACK_NAME}
+aws cloudformation deploy --template-file target/output-sam.yaml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM
+aws cloudformation describe-stacks --stack-name ${STACK_NAME}
