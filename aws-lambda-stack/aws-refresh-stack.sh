31751a878b0fc40b0b910dbf5f6b20d4d9ddc5f1
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-10-24 14:07:58 +0200

    Refactor

--- aws-lambda-stack/aws-refresh-stack.sh
+++ aws-lambda-stack/aws-refresh-stack.sh
@@ -3,11 +3,31 @@
 set -e
 
 STACK_NAME=spring-boot-on-lambda-demo
-REPO_ROOT=$(dirname $0)
+PROG_DIR=$(dirname $0)
+BACKEND_DIR=${PROG_DIR}/../backend
+JAR_FILE=accenture-microservices-demo-0.0.1-SNAPSHOT.jar
 
-cd ${REPO_ROOT}
-./mvnw -P aws-serverless -D skipTests=true clean package
-aws cloudformation delete-stack --stack-name ${STACK_NAME}
-aws cloudformation package --template-file sam.yaml --output-template-file target/output-sam.yaml --s3-bucket ${STACK_NAME}
-aws cloudformation deploy --template-file target/output-sam.yaml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM
+cleanup()
+{
+  echo TRAPPING
+  rm -f ${JAR_FILE}
+}
+
+trap cleanup INT TERM EXIT
+
+make -C ${BACKEND_DIR} clean-aws-lambda package-aws-lambda
+
+aws s3 mb s3://spring-boot-on-lambda-demo
+
+if aws cloudformation delete-stack --stack-name ${STACK_NAME}
+then
+  sleep 10
+fi
+
+cd ${PROG_DIR}
+cp ${BACKEND_DIR}/target/${JAR_FILE} .
+
+aws cloudformation package --template-file sam.yaml --output-template-file output-sam.yaml --s3-bucket ${STACK_NAME}
+aws cloudformation deploy --template-file output-sam.yaml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM
+
 aws cloudformation describe-stacks --stack-name ${STACK_NAME}
