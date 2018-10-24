cd8c0e5ad624b03ea06049ce4a0c0ce06c00361e
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-10-24 13:09:03 +0200

    Add clean, build and package targets to Makefile

--- backend/local.mk
+++ backend/local.mk
@@ -1,2 +1,21 @@
-prova:
-	echo test
+MVN=                 ./mvnw
+PROFILE_SPRING_BOOT= spring-boot
+PROFILE_AWS_LAMBDA=  aws-serverless
+
+clean-spring-boot:
+	$(MVN) -P $(PROFILE_SPRING_BOOT) clean
+
+build-spring-boot:
+	$(MVN) -P $(PROFILE_SPRING_BOOT) compile
+
+package-spring-boot:
+	$(MVN) -P $(PROFILE_SPRING_BOOT) -DskipTests=true package
+
+clean-aws-lambda:
+	$(MVN) -P $(PROFILE_AWS_LAMBDA) clean
+
+build-aws-lambda:
+	$(MVN) -P $(PROFILE_AWS_LAMBDA) compile
+
+package-aws-lambda:
+	$(MVN) -P $(PROFILE_AWS_LAMBDA) -DskipTests=true package
