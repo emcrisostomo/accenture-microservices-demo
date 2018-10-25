667ce295e0fa719f3294f45d393aaf82e6d9fd52
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-10-25 10:55:52 +0200

    Add ecs-compose-down.sh

---
+++ aws-ecs-fargate/ecs-compose-down.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+set -u
+
+PROG_DIR=$(dirname $0)
+
+. ${PROG_DIR}/ecs-cli.conf
+
+ecs-cli compose \
+  --file stack.yaml \
+  --project-name ${CLUSTER_NAME} \
+  service down \
+    --cluster-config ${CLUSTER_NAME}
