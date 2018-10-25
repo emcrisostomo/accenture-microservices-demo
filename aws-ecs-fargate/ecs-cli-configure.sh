5033d3f7e3661e7d5ba262dad898816a0ec7741e
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-10-25 10:02:05 +0200

    Refactor to use variables

--- aws-ecs-fargate/ecs-cli-configure.sh
+++ aws-ecs-fargate/ecs-cli-configure.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 set -e
+set -u
 
 PROG_DIR=$(dirname $0)
 
 . ${PROG_DIR}/ecs-cli.conf
 
 ecs-cli configure \
-  --region eu-east-1 \
+  --region ${AWS_REGION} \
   --cluster ${CLUSTER_NAME} \
   --default-launch-type FARGATE \
   --config-name ${CLUSTER_NAME}
