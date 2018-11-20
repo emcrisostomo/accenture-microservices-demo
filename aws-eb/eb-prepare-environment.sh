47b8236ef15c9a692558b36d7012f96e6cd08550
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-11-20 12:35:50 +0100

    Fix profile used to build Elastic Beanstalk artifacts

--- aws-eb/eb-prepare-environment.sh
+++ aws-eb/eb-prepare-environment.sh
@@ -6,6 +6,6 @@ PROG_DIR=$(dirname $0)
 BACKEND_DIR=${PROG_DIR}/../backend
 JAR_FILE=accenture-microservices-demo-0.0.1-SNAPSHOT.jar
 
-make -C ${BACKEND_DIR} clean-aws-lambda package-aws-lambda
+make -C ${BACKEND_DIR} clean-spring-boot package-spring-boot
 cd ${PROG_DIR}
 cp ${BACKEND_DIR}/target/${JAR_FILE} .
